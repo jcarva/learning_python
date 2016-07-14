99069d0b821927ff6a61b9348dea521119bf23b3
Jaelson <jaelsonjunior99@gmail.com>
2016-07-14 07:56:09 -0300

    Update functions

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 import pyautogui
-import time
 import threading
+import time
 import sys
+import frank
 
 functions = ['func1', 'func2', 'func3', 'func4']
 
 COUNTDOWN = 5
 lock = threading.Lock()
+
+class Process(threading.Thread):
+
+    def __init__(self, name):
+        sys.stdout.write("Making thread name " + str(name) + "\n")
+        sys.stdout.flush()
+        threading.Thread.__init__(self)
+        self.name = name
+        self.countdown = COUNTDOWN
+
+    def run(self):
+
+        # while True: #Running in Loop
+        lock.acquire()
+        eval(self.name)(self.countdown, self.name)
+        lock.release()
+
+
+def main():
+    for thread_name in functions:
+        thread = Process(thread_name)
+        thread.start()
 
 def func1(countdown, name):
     while (countdown):
@@ -36,23 +59,5 @@ def func4(countdown, name):
         sys.stdout.flush()
         countdown -= 1
 
-
-class Process(threading.Thread):
-
-    def __init__(self, name):
-        sys.stdout.write("Making thread name " + str(name) + "\n")
-        sys.stdout.flush()
-        threading.Thread.__init__(self)
-        self.name = name
-        self.countdown = COUNTDOWN
-
-    def run(self):
-
-        # while True: #Running in Loop
-        lock.acquire()
-        eval(self.name)(self.countdown, self.name)
-        lock.release()
-
-for thread_name in functions:
-    thread = Process(thread_name)
-    thread.start()
+if __name__ == '__main__':
+    main()
