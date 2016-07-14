47e9a3a3585e1cd45f0b2a925d00616d2133f311
Jaelson <jaelsonjunior99@gmail.com>
2016-07-14 08:44:03 -0300

    Create classes to input the logic

--- frank.py
+++ frank.py
@@ -1,30 +1,22 @@
 import time
 import sys
+from func1 import Func1
+from func2 import Func2
+from func3 import Func3
+from func4 import Func4
 
 def func1(countdown, name):
-    while (countdown):
-        time.sleep(0.3)
-        sys.stdout.write("Thread " + str(name) + " (" + str(countdown) + ")\n")
-        sys.stdout.flush()
-        countdown -= 1
+	func1 = Func1(countdown, name)
+	func1.exe()
 
 def func2(countdown, name):
-    while (countdown):
-        time.sleep(0.3)
-        sys.stdout.write("Thread " + str(name) + " (" + str(countdown) + ")\n")
-        sys.stdout.flush()
-        countdown -= 1
+	func2 = Func1(countdown, name)
+	func2.exe()
 
 def func3(countdown, name):
-    while (countdown):
-        time.sleep(0.3)
-        sys.stdout.write("Thread " + str(name) + " (" + str(countdown) + ")\n")
-        sys.stdout.flush()
-        countdown -= 1
+	func3 = Func1(countdown, name)
+	func3.exe()
 
 def func4(countdown, name):
-    while (countdown):
-        time.sleep(0.3)
-        sys.stdout.write("Thread " + str(name) + " (" + str(countdown) + ")\n")
-        sys.stdout.flush()
-        countdown -= 1
+	func4 = Func1(countdown, name)
+	func4.exe()
---
+++ func1.py
@@ -0,0 +1,14 @@
+import time
+import sys
+
+class Func1:
+	def __init__(self, countdown, name):
+		self.countdown = countdown
+		self.name = name
+
+	def exe( self ):
+	    while (self.countdown):
+	        time.sleep(0.3)
+	        sys.stdout.write("Thread " + str(self.name) + " (" + str(self.countdown) + ")\n")
+	        sys.stdout.flush()
+	        self.countdown -= 1
